36611b00789ab8cc263281666d9f59213ed969ec
caosihui <sanjinzhuzhu@aliyun.com>
2023-08-01 16:41:54 +0800

    init config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+require("nvim-treesitter.install").prefer_git = true;
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { "vim", "help", "bash", "c", "cpp", "javascript", "json", "lua", "java", "python", "typescript", "rust", "vue", "css", "html", "tsx"},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  -- sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  -- auto_install = true,
+
+  -- List of parsers to ignore installing (for "all")
+  -- ignore_install = { "javascript" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+  },
+
+  indent = { enable = true },
+
+  -- 不同括号颜色区分
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil
+  }
+}
