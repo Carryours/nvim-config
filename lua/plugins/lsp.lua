94685ce19fd11e470489287febaf9afb93ac8cdc
彭鑫 <pengxin@MacBook-Pro-pengxin.local>
2024-11-27 10:19:55 +0800

    feat: add volar config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,12 +12,16 @@ require("mason-lspconfig").setup({
   -- 确保安装，根据需要填写
   ensure_installed = {
     "lua_ls",
+    "volar",
+    "ts_ls"
   },
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").lua_ls.setup {
+local lspconfig = require("lspconfig")
+
+lspconfig.lua_ls.setup {
   capabilities = capabilities,
   settings = {
     Lua = {
@@ -39,4 +43,31 @@ require("lspconfig").lua_ls.setup {
       },
     },
   }
+}
+
+
+local mason_registry = require('mason-registry')
+
+local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path()..'/node_modules/@vue/language-server'
+lspconfig.ts_ls.setup {
+  init_options = {
+    plugins = {
+      {
+        name = '@vue/typescript-plugin',
+        location = vue_language_server_path,
+        languages = { 'vue' },
+      }
+    },
+  },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+}
+
+lspconfig.volar.setup = {
+
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  init_options = {
+    vue = {
+      hybridMode = false,
+    },
+  },
 }
